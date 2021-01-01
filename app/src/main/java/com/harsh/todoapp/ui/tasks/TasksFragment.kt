d861b6a24ae8e3d598b9b4301b615e7e5e7bf558
hArsh <123321pudaleharshal@gmail.com>
2021-01-01 20:09:29 +0530

    TaskEventChannel added

--- app/src/main/java/com/harsh/todoapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/harsh/todoapp/ui/tasks/TasksFragment.kt
@@ -10,12 +10,16 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.harsh.todoapp.R
 import com.harsh.todoapp.data.Task
 import com.harsh.todoapp.databinding.FragmentTasksBinding
 import com.harsh.todoapp.util.OnQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
@@ -39,11 +43,41 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), OnTaskClickListener {
                 layoutManager = LinearLayoutManager(requireContext())
                 setHasFixedSize(true)
             }
+            ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+                1,
+                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+            ) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val task = taskadapter.currentList[viewHolder.adapterPosition]
+                    viewModel.onTaskSwiped(task)
+                }
+            }).attachToRecyclerView(recyclerview)
         }
 
         viewModel.tasks.observe(viewLifecycleOwner, Observer {
             taskadapter.submitList(it)
         })
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.tasksEvent.collect { event ->
+                when (event) {
+                    is TasksViewModel.TasksEvent.ShowUndoDeleteTaskMessage -> {
+                        Snackbar.make(requireView(), "Task deleted", Snackbar.LENGTH_LONG)
+                            .setAction("UNDO") {
+                                viewModel.onUndoDeleteClick(event.task)
+                            }.show()
+                    }
+                }
+            }
+        }
 
     }
 
--- app/src/main/java/com/harsh/todoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/harsh/todoapp/ui/tasks/TasksViewModel.kt
@@ -7,9 +7,11 @@ import androidx.lifecycle.viewModelScope
 import com.harsh.todoapp.data.PreferenceManager
 import com.harsh.todoapp.data.Task
 import com.harsh.todoapp.data.TaskDao
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 class TasksViewModel @ViewModelInject constructor(
@@ -17,10 +19,12 @@ class TasksViewModel @ViewModelInject constructor(
     private val preferenceManager: PreferenceManager
 ) : ViewModel() {
     val searchQuery = MutableStateFlow("")
-/*    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
-    val hideCompleted = MutableStateFlow(false)*/
 
     val preferencesFlow = preferenceManager.preferencesFlow
+
+    private val tasksEventChannel = Channel<TasksEvent>()
+
+    val tasksEvent = tasksEventChannel.receiveAsFlow()
 
 
     private val tasksFlow = combine(
@@ -48,6 +52,23 @@ class TasksViewModel @ViewModelInject constructor(
         viewModelScope.launch {
             taskDao.update(task.copy(completed_state = isChecked))
         }
+
+    fun onTaskSwiped(task: Task) {
+        viewModelScope.launch {
+            taskDao.delete(task)
+            tasksEventChannel.send(TasksEvent.ShowUndoDeleteTaskMessage(task))
+        }
+    }
+
+    fun onUndoDeleteClick(task: Task) {
+        viewModelScope.launch {
+            taskDao.insert(task)
+        }
+    }
+
+    sealed class TasksEvent {
+        data class ShowUndoDeleteTaskMessage(val task: Task) : TasksEvent()
+    }
 
 
 }
